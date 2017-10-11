acd665af033fbb627b917060c4c4de317aedc261
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-10 21:10:42 -0700

    Create source and header file for Astar class, which defined the Astar algorithm -User Story # 6

---
+++ include/Astar.hpp
@@ -0,0 +1,41 @@
+/**
+*@file WeightedAstar.h
+*@Copyright (C) 2017 Zejiang Zeng - All Rights Reserved
+* You may use, distribute and modify this code under the
+* terms of the MIT license, please visit :
+ https://github.com/zzjkf2009/Acme-Robotics-Project/blob/master/LICENSE
+*@brief This header define the <Astar> calss, which is an algorithm for path
+*        planning
+*/
+
+#ifndef INCLUDE_ASTAR_H_
+#define INCLUDE_ASTAR_H_
+
+#include<iostream>
+#include<set>
+#include<vector>
+#include<stack>
+#include<cmath>
+#include<cstring>
+#include<utility>
+
+class Astar {
+ public:
+#define ROW 10
+#define COL 10
+#define FLT_MAX __FLT_MAX__
+  struct node {
+    int parent_x;
+    int parent_y;
+    double F, H, G;
+  };
+  typedef std::pair<int, int> coordinate;
+  typedef std::pair<double, std::pair<int, int> > Open_list;
+  bool isValid(const int& x, const int& y);
+  bool isUnblocked(int grid[][COL], const int& x, const int& y);
+  bool isGoalNode(const int& x, const int& y, coordinate goal);
+  double calculate_H(const int& x, const int& y, coordinate goal);
+  std::stack<coordinate> GeneratePath(node nodepath[][COL], coordinate goal);
+};
+
+#endif  // INCLUDE_ASTAR_H_
