16ff312e39cca7abec49d46698a2378d35b9f14e
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-10 21:56:38 -0700

    Fix the bug when load map info by using std::array -User Story # 5

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <lib.hpp>
-
+#include <array>
+#include <iostream>
+#include "Astar.hpp"
+#include "Map.hpp"
 int main()
 {
-    dummy();
+
+   int x = 1;
+   Map map;
+
+std::array<std::array<int, 10>, 10> map_info = map.getGridmap(x);
+
+
+std::cout<<arr[1][1]<<std::endl;
+
     return 0;
 }
