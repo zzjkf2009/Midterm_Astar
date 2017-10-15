743ab9d460bc1d41ad601efd413968fe13158978
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-15 14:35:34 -0700

    Modified main file for debugging

--- app/main.cpp
+++ app/main.cpp
@@ -3,16 +3,18 @@
 #include <iostream>
 #include "Astar.hpp"
 #include "Map.hpp"
+#include "WeightedAstar.hpp"
 int main()
 {
 
-   int x = 1;
-   Map map;
 
-Map::gridMatrix map_info = map.getGridmap(x);
-
-
-std::cout<<map_info[1][1]<<std::endl;
-
-    return 0;
+   Map setmap;
+   Map::gridMatrix map_info = setmap.getGridmap(3);
+   Astar::coordinate start = setmap.SetStart(0, 4);
+   Astar::coordinate goal = setmap.SetGoal(9, 5);
+   std::stack<Astar::coordinate> Path;
+   int weight = 2;
+   WeightedAstar w_astar;
+   Path = w_astar.WeightedA(map_info, start, goal, weight);
+   return 0;
 }
