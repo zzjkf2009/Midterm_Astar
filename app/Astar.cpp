6f948a264dc013b0de17cc0289d82db10f93f44e
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-17 21:09:46 -0700

    Update Astar class, Remove unused headers, Using  c++ style static constant to define a constant instead of c style. Modified the comment in Doxygen style -User Story #6

--- app/Astar.cpp
+++ app/Astar.cpp
@@ -3,71 +3,43 @@
 *@Copyright (C) 2017 Zejiang Zeng - All Rights Reserved
 * You may use, distribute and modify this code under the
 * terms of the MIT license, please visit :
- https://github.com/zzjkf2009/Acme-Robotics-Project/blob/master/LICENSE
+ https://github.com/zzjkf2009/Midterm_Astar/blob/master/LICENSE
 *@brief This file define several functions of <Astar> class
 *
 */
 #include<iostream>
-#include<set>
-#include<vector>
 #include<stack>
-#include"Astar.hpp"
-#include"Map.hpp"
 #include<cmath>
-#include<utility>
+#include"Astar.hpp"
 
-/*@brief define a data structure contains two parameters that a node may have */
-typedef std::pair<int, int> coordinate;
 
-/**
-*@brief Function that checks if the input coordinate is inside the grid map
-*@param two integars
-*@return ture or false
-*/
+
+
 bool Astar::isValid(const int& x, const int& y) {
   return ((x >= 0) && (x < ROW) && (y >= 0) && (y < COL));
 }
-/**
-*@brief  Function that checks if the grid is a block or wall something like that
-*@param  grid[10][10], coordinate x and y, int
-*@return boolean ture or false
-*/
-bool Astar::isUnblocked(Map::gridMatrix grid, const int& x, const int& y) {
+
+bool Astar::isUnblocked(gridMatrix grid, const int& x, const int& y) {
   if (grid[x][y] == 1)
     return true;
   else
     return false;
 }
-/**
-*@  Funtion that checks if it is the goal node
-*@param two int x and y, goal coordinate
-*@return boolean,ture or false
- */
+
 bool Astar::isGoalNode(const int& x, const int& y, coordinate goal) {
   if (x == goal.first && y == goal.second)
     return true;
   else
     return false;
 }
-/**
-*@brief  Function that calculates the heuristic value between current node and
-*goal node method usd here is Eucliden Distance. Other method like Diagnal
-* Distance or Manhattan Distance are also appliable
-*@param two int x and y, coordinate of gaol
-*@return doule
-*/
+
 double Astar::calculate_H(const int& x, const int& y, coordinate goal) {
   return  sqrt(
       (x - goal.first) * (x - goal.first)
           + (y - goal.second) * (y - goal.second));
 }
-/**
-*@brief Function that will generate the path back from goal node to the start node
- and the path of the nodes will displayed on the screen
-*@param nodepath and goal coordinate
-*@return stack<coordinate>
- */
-std::stack<coordinate> Astar::GeneratePath(node nodepath[][COL],
+
+std::stack<Astar::coordinate> Astar::GeneratePath(node nodepath[][COL],
                                            coordinate goal) {
   printf("The path is  \n");
   int x = goal.first;
--- include/Astar.hpp
+++ include/Astar.hpp
@@ -1,5 +1,5 @@
 /**
-*@file WeightedAstar.h
+*@file WeightedAstar.hpp
 *@Copyright (C) 2017 Zejiang Zeng - All Rights Reserved
 * You may use, distribute and modify this code under the
 * terms of the MIT license, please visit :
@@ -12,19 +12,16 @@
 #define INCLUDE_ASTAR_H_
 
 #include<iostream>
-#include<set>
-#include<vector>
 #include<stack>
-#include<cmath>
-#include<cstring>
-#include<utility>
-#include "Map.hpp"
+#include<limits>
+#include<array>
+
 
 class Astar {
- public:
-#define ROW 10
-#define COL 10
-#define FLT_MAX __FLT_MAX__
+public:
+const double MAX = std::numeric_limits<double>::max();
+static const int ROW = 10;
+static const int COL = 10;
   struct node {
     int parent_x;
     int parent_y;
@@ -32,10 +29,47 @@ class Astar {
   };
   typedef std::pair<int, int> coordinate;
   typedef std::pair<double, std::pair<int, int> > Open_list;
+  typedef std::array<std::array<int, 10>, 10> gridMatrix;
+/**
+*@brief Method that checks if the input coordinate is inside the grid map
+*@param int <x> location at x axis 
+*@param int <y>, location at y axis
+*@return bool type ture or false
+*/
   bool isValid(const int& x, const int& y);
-  bool isUnblocked(Map::gridMatrix grid, const int& x, const int& y);
+/**
+*@brief  Method that checks if the grid is a block or wall something like that
+*@param  <grid> a matrix created using array of array
+*@param <x> int x coordinate 
+*@param <y> int y coordinatesss
+*@return bool ture or false
+*/
+  bool isUnblocked(gridMatrix grid, const int& x, const int& y);
+/**
+*@brief  Method that checks if the current node is the goal node
+*@param <goal> corrdinate of goal in pair
+*@param <x> int x coordinate 
+*@param <y> int y coordinatesss
+*@return bool ture or false
+*/
   bool isGoalNode(const int& x, const int& y, coordinate goal);
+/**
+*@brief  Function that calculates the heuristic value between current node and
+*goal node method usd here is Eucliden Distance. Other method like Diagnal
+* Distance or Manhattan Distance are also appliable
+*@param <x> int x coordinate 
+*@param <y> int y coordinate
+*@param <goal> corrdinate of goal in pair
+*@return doule f value
+*/
   double calculate_H(const int& x, const int& y, coordinate goal);
+/**
+*@brief Function that will generate the path back from goal node to the start node
+ and the path of the nodes will displayed on the screen
+*@param <node> the path in terms of each node location
+*@param <goal> goal coordinate
+*@return stack<coordinate>
+ */
   std::stack<coordinate> GeneratePath(node nodepath[][COL], coordinate goal);
 };
 
