cce84f22ca33fff9859de9dd689d5c6d6278176b
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-10-17 21:14:06 -0700

    Add self-defined type, instead of inclding other headers, Modified the comment in Doxygen style -User story #5

--- app/Map.cpp
+++ app/Map.cpp
@@ -3,7 +3,7 @@
 *@Copyright (C) 2017 Zejiang Zeng - All Rights Reserved
 * You may use, distribute and modify this code under the
 * terms of the MIT license, please visit :
- https://github.com/zzjkf2009/Acme-Robotics-Project/blob/master/LICENSE
+ https://github.com/zzjkf2009/Midterm_Astar/blob/master/LICENSE
 *@brief This file define two functions of <Map> class
 */
 
@@ -11,7 +11,7 @@
 #include<array>
 
 
-Map::gridMatrix Map::getGridmap(const int index) {
+Map::gridMatrix Map::getGridmap(const int& index) {
         Map::gridMatrix gridMap_1 =  {
         {
             {1,1,1,1,1,1,1,1,1,1},
@@ -86,24 +86,10 @@ Map::gridMatrix Map::getGridmap(const int index) {
          }
 }
 
-
-
-
-
-/**
-*@brief This function read two integar and make them as the 2D coordinate of the
-*       node
-*@param x and y, both are int, which represent the location of the start node
-*@return coordinate pair<x,y>
-*/
 Map::coordinate Map::SetStart(const int& x, const int& y) {
   return std::make_pair(x, y);
 }
-/**
-*@brief Same as last funtion, it returns the coordinate of goal node
-*@param two integars x and y
-*@return coordinate pair<x,y>
-*/
+
 Map::coordinate Map::SetGoal(const int& x, const int& y) {
   return std::make_pair(x, y);
 }
--- include/Map.hpp
+++ include/Map.hpp
@@ -1,10 +1,10 @@
 /**
-*@file Map.h
+*@file Map.hpp
 *@Copyright (C) 2017 Zejiang Zeng - All Rights Reserved
 * You may use, distribute and modify this code under the
 * terms of the MIT license, please visit :
  https://github.com/zzjkf2009/Acme-Robotics-Project/blob/master/LICENSE
-*@brief This header define the <Map> calss, which is to read start and goal node
+*@brief This header define the <Map> calss, which is to select a pre-defined grid map and read start, goal node
 */
 
 #ifndef INCLUDE_MAP_H_
@@ -13,12 +13,31 @@
 
 #include<utility>
 #include<array>
+
 class Map{
  public:
 typedef std::array<std::array<int, 10>, 10> gridMatrix;
 typedef std::pair<int, int> coordinate;
-gridMatrix getGridmap(const int index);
+/**
+*@brief This method is to choose a self-defined grid map
+*@param <index> int a index of the grid map
+*@return a matrix of the map built by array of array
+*/
+gridMatrix getGridmap(const int& index);
+/**
+*@brief This function read two integar and make them as the 2D coordinate of the
+*       node
+*@param int <x> location at x axis 
+*@param int <y>, location at y axis
+*@return coordinate pair<x,y>
+*/
 coordinate SetStart(const int& x, const int& y);
+/**
+*@brief Same as last funtion, it returns the coordinate of goal node
+*@param int <x> location at x axis 
+*@param int <y>, location at y axis
+*@return coordinate pair<x,y>
+*/
 coordinate SetGoal(const int& x, const int& y);
 };
 
